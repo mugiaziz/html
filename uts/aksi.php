ca8baf9c26a7aa505995c2ca34ab8709ddefb7b8
mugiaziz <92796523+mugiaziz@users.noreply.github.com>
2021-10-19 19:53:26 +0700

    Add files via upload

---
+++ uts/aksi.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Validasi</title>
+</head>
+<body>
+<?php
+echo "<h2>Hallo $_POST[username] </h2>";
+?>
+<h2>Data diri anda adalah : </h2>
+<?php
+echo "Password anda adalah ";
+echo "$_POST[alamat]<br>";
+echo "Email anda adalah ";
+echo "<font color='blue'>$_POST[email]</font>";
+?>
+</body>
+</html>
